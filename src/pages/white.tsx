b8b6f0720a9cbf48456721dc5b08c80e64eeb057
Casey Bradford <caseyebradford@gmail.com>
2022-02-16 21:10:42 -0500

    changes

--- src/pages/white.tsx
+++ src/pages/white.tsx
@@ -9,14 +9,23 @@ import { pxToRem } from '~/logic/util/styles';
 const Background = styled(FlexBox)`
   min-height: 100%;
   width: 100%;
+  background-color: #fbf2ea;
 `;
 
 const FunButton = styled(Button)`
-  background-color: white;
-  border: ${pxToRem(2)} solid blue;
+  background-color: #fbf2ea;
+  border: ${pxToRem(2)} solid black;
   padding: ${pxToRem(15)} ${pxToRem(100)};
   border-radius: ${pxToRem(100)};
   transform: translate(10vw, -10vh);
+  font-size: 2em;
+`;
+
+const ButtonShadow = styled.div`
+  background-color: black;
+  border-radius: 20px 20px 50px 50px;
+  width: 100px;
+  height: 100px;
 `;
 
 const White: React.FC = () => {
@@ -26,8 +35,9 @@ const White: React.FC = () => {
     <Layout>
       <Background center>
         <FunButton onClick={push}>
-          <p>Click Me</p>
+          <p>click me!</p>
         </FunButton>
+        <ButtonShadow />
       </Background>
     </Layout>
   );
