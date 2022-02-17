7b335e3956ea0d0bfe851f0fa1e36f795a593aa8
Casey Bradford <caseyebradford@gmail.com>
2022-02-17 15:11:44 -0500

    push down button

--- src/pages/white.tsx
+++ src/pages/white.tsx
@@ -1,10 +1,15 @@
+import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
+import { useState } from 'react';
 
 import { FlexBox } from '~/components/box/FlexBox';
 import { Button } from '~/components/Button';
 import { Layout } from '~/components/meta/Layout';
 import { useGetPushFn } from '~/logic/routing';
-import { pxToRem } from '~/logic/util/styles';
+import { delay } from '~/logic/util/delay';
+import { pxToRem, toggleAnimation } from '~/logic/util/styles';
+
+const animationTimer = 2000;
 
 const Background = styled(FlexBox)`
   min-height: 100%;
@@ -12,32 +17,49 @@ const Background = styled(FlexBox)`
   background-color: #fbf2ea;
 `;
 
-const FunButton = styled(Button)`
-  background-color: #fbf2ea;
-  border: ${pxToRem(2)} solid black;
-  padding: ${pxToRem(15)} ${pxToRem(100)};
-  border-radius: ${pxToRem(100)};
+const ButtonWrapper = styled.div`
   transform: translate(10vw, -10vh);
-  font-size: 2em;
 `;
 
-const ButtonShadow = styled.div`
-  background-color: black;
-  border-radius: 20px 20px 50px 50px;
-  width: 100px;
-  height: 100px;
+const buttonDown = keyframes`
+  from {
+    box-shadow: 0px 20px 0px 0px #000000;
+    transform: translateY(0)
+  }
+  to {
+    box-shadow: 0px 0px 0px 0px #000000;
+    transform: translateY(20px);
+  }
+`;
+
+const FunButton = styled(Button)<{ isTransitioning: boolean }>`
+  background-color: #fbf2ea;
+  border: ${pxToRem(4)} solid black;
+  padding: ${pxToRem(15)} ${pxToRem(100)};
+  border-radius: ${pxToRem(100)};
+  font-size: 2em;
+  box-shadow: 0px 20px 0px 0px #000000;
+  ${({ isTransitioning }) => toggleAnimation(buttonDown, 500, isTransitioning)}
 `;
 
 const White: React.FC = () => {
+  const [isTransitioning, setIsTransitioning] = useState(false);
   const push = useGetPushFn();
+
+  const onClick = async () => {
+    setIsTransitioning(true);
+    await delay(null, animationTimer);
+    push();
+  };
 
   return (
     <Layout>
       <Background center>
-        <FunButton onClick={push}>
-          <p>click me!</p>
-        </FunButton>
-        <ButtonShadow />
+        <ButtonWrapper>
+          <FunButton isTransitioning={isTransitioning} onClick={onClick}>
+            <p>click me!</p>
+          </FunButton>
+        </ButtonWrapper>
       </Background>
     </Layout>
   );
