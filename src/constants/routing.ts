9fa8a696ddee1c1b2c90d754f9186196247e20a4
Casey Bradford <caseyebradford@gmail.com>
2022-02-24 13:44:17 -0500

    circle button

--- src/constants/routing.ts
+++ src/constants/routing.ts
@@ -1,3 +1,10 @@
 export const HOME_ROUTE = '/';
 
-export const BUTTON_ROUTES = ['blue-bg', 'beige-three-d', 'loading', 'gradient', 'spin'];
+export const BUTTON_ROUTES = [
+  'blue-bg',
+  'beige-three-d',
+  'loading',
+  'gradient',
+  'spin',
+  'circle',
+];
